90ffdb8dca9e40de860cd2c4cb584e5f73fc77e4
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-21 16:59:59 +0530

    update in layout and added header

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 export const metadata = {
   title: "Eventify",
@@ -10,7 +11,17 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
+          {children}
+        </main>
+        <footer className="py-12 bg-blue-100">
+          <div className="container px-4 mx-auto text-center text-gray-600">
+            <p>Made with 💗 by Praveen Raj</p>
+          </div>
+        </footer>
+      </body>
     </html>
   );
 }
---
+++ components/header.jsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { PenBox } from "lucide-react";
+// import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+
+function Header() {
+  return (
+    <nav className="flex items-center justify-between px-4 py-2 mx-auto border-b-2 shadow-md">
+      <Link href="/" className="flex items-center">
+        <Image
+          src="/logo.png"
+          width="150"
+          height="60"
+          alt="Schedulrr Logo"
+          className="w-auto h-16"
+        />
+      </Link>
+
+      <div className="flex items-center gap-4">
+        <Link href="/events?create=true">
+          <Button variant="default" className="flex items-center gap-2">
+            <PenBox size={18} />
+            <span className="hidden sm:inline">Create Event</span>
+          </Button>
+        </Link>
+        <Button variant="outline">Login</Button>
+        {/* <SignedOut>
+          <SignInButton forceRedirectUrl="/dashboard">
+            <Button variant="outline">Login</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserMenu />
+        </SignedIn> */}
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
