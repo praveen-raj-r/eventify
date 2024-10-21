f99c33c4a26571131d07837de761ea332e97f5ae
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-21 17:33:05 +0530

    added middleware

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata = {
   title: "Eventify",
@@ -10,18 +11,20 @@ export const metadata = {
 const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header />
-        <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
-          {children}
-        </main>
-        <footer className="py-12 bg-blue-100">
-          <div className="container px-4 mx-auto text-center text-gray-600">
-            <p>Made with 💗 by Praveen Raj</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Header />
+          <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
+            {children}
+          </main>
+          <footer className="py-12 bg-blue-100">
+            <div className="container px-4 mx-auto text-center text-gray-600">
+              <p>Made with 💗 by Praveen Raj</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
