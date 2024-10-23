a2a96d730440306ad58971b3da121eb55d7002bc
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-23 14:34:27 +0530

    added dashboard

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,6 +1,66 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { useUser } from "@clerk/nextjs";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { usernameSchema } from "@/app/lib/validators";
+import { useEffect } from "react";
+
 function Dashboard() {
-    return <div>Dashboard content</div>;
-  }
-  
-  export default Dashboard;
-  
+  const { isLoaded, user } = useUser();
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(usernameSchema),
+  });
+
+  useEffect(() => {
+    setValue("username", user?.username);
+  }, [isLoaded]);
+
+  const onSubmit = async (data) => {
+    await fnUpdateUsername(data.username);
+  };
+
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Welcome, {user?.firstName}!</CardTitle>
+        </CardHeader>
+        <CardContent></CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Your Unique Link</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            <div>
+              <div className="flex items-center gap-2">
+                <span>{window?.location.origin}/</span>
+                <Input {...register("username")} placeholder="username" />
+              </div>
+              {errors.username && (
+                <p className="mt-1 text-sm text-red-500">
+                  {errors.username.message}
+                </p>
+              )}
+            </div>
+            <Button type="submit" disabled={false}>
+              Update Username
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+export default Dashboard;
--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,8 +1,7 @@
 export default function NotFound() {
-    return (
-      <div className="grid w-screen text-4xl font-extrabold pt-96 place-items-center">
-        <h1>404 - Page Not Found</h1>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="grid w-screen text-4xl font-extrabold pt-96 place-items-center">
+      <h1>404 - Page Not Found</h1>
+    </div>
+  );
+}
