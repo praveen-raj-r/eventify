6e31ff796eabd76ec34da6030f27b5548ce42aa4
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-23 12:29:53 +0530

    added not found page

---
+++ app/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <div className="grid w-screen text-4xl font-extrabold pt-96 place-items-center">
+        <h1>404 - Page Not Found</h1>
+      </div>
+    );
+  }
+  
