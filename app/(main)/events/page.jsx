73b4d2f6cff91d306ebcc410348c03fbe8472dac
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-24 16:54:21 +0530

    updated events page

--- app/(main)/events/page.jsx
+++ app/(main)/events/page.jsx
@@ -1,6 +1,26 @@
-function Events() {
-    return <div>Events</div>;
+import { getUserEvents } from "@/actions/events";
+import EventCard from "@/components/event-card";
+import { Suspense } from "react";
+
+export default function EventsPage() {
+  return (
+    <Suspense fallback={<div>Loading events...</div>}>
+      <Events />
+    </Suspense>
+  );
+}
+
+async function Events() {
+  const { events, username } = await getUserEvents();
+
+  if (events.length === 0) {
+    return <p>You haven&apos;t created any events yet.</p>;
   }
-  
-  export default Events;
-  
+  return (
+    <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+      {events?.map((event) => (
+        <EventCard key={event.id} event={event} username={username} />
+      ))}
+    </div>
+  );
+}
